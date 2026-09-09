fac186bdd5b3732a6f83166c1bd4f8021ec3c19a
sharifunshaik950-alt <sharifunshaik950@gmail.com>
2026-09-09 20:06:19 +0530

    fix test and jenkinsfile

--- test_addition.py
+++ test_addition.py
@@ -1,12 +1,12 @@
 import addition
+import math
 
-assert addition.add(5, 10) == 15
-assert addition.add(0, 0) == 0
-assert addition.add(-5, 5) == 0
-assert addition.add(-3, -7) == -10
-assert addition.add(2.5, 3.5) == 6.0
-assert addition.add(1e10, 1e10) == 2e10
-assert addition.add(1, -1) == 0
-assert addition.add(100, 200) == 300
-assert addition.add(0.1, 0.2) == 0.3
-
+assert addition.add(5, 10) == 15[cite: 9]
+assert addition.add(0, 0) == 0[cite: 9]
+assert addition.add(-5, 5) == 0[cite: 9]
+assert addition.add(-3, -7) == -10[cite: 9]
+assert addition.add(2.5, 3.5) == 6.0[cite: 9]
+assert addition.add(1e10, 1e10) == 2e10[cite: 9]
+assert addition.add(1, -1) == 0[cite: 9]
+assert addition.add(100, 200) == 300[cite: 9]
+assert math.isclose(addition.add(0.1, 0.2), 0.3)
